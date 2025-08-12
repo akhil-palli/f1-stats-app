2056f0949b864b3290648c401612df39b6072d6d
akhil-palli <akhildpalli@gmail.com>
2025-08-11 23:03:22 -0400

    build errors

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import type { FC } from 'react';
+import { F1RealDataDashboard } from '@/components/F1RealDataDashboard';
+
+const DashboardPage: FC = () => {
+  return <F1RealDataDashboard />;
+};
+
+export default DashboardPage;
